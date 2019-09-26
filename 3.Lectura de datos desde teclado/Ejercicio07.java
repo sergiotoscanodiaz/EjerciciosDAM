a3e71d9b2d746991ede668a233772245e4cd8e69
Alan Brito Delgado <alan.brito.delgado.1972@gmail.com>
2019-09-26 17:44:21 +0200

    Ejercicio07 del tema 3 terminado

---
+++ 3.Lectura de datos desde teclado/Ejercicio07.java
@@ -0,0 +1,12 @@
+public class Ejercicio07 {
+  public static void main(String[] args) {
+
+    System.out.print("Por favor, introduzca la base imponible : ");
+    double baseImponible = Double.parseDouble(System.console().readLine());
+
+    System.out.printf("Base imponible %8.2f\n", baseImponible);
+    System.out.printf("IVA            %8.2f\n", (baseImponible * 0.21));
+    System.out.printf("-------------------\n");
+    System.out.printf("Total          %8.2f\n", (baseImponible * 1.21));
+  }
+}
