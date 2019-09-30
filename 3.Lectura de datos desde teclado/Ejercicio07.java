075c65bc847e7c2c401578847b0bc0033d209561
Alan Brito Delgado <alan.brito.delgado.1972@gmail.com>
2019-09-30 09:34:10 +0200

    README.md actualizado

--- 3.Lectura de datos desde teclado/Ejercicio07.java
+++ 3.Lectura de datos desde teclado/Ejercicio07.java
@@ -4,7 +4,7 @@ public class Ejercicio07 {
     System.out.print("Por favor, introduzca la base imponible : ");
     double baseImponible = Double.parseDouble(System.console().readLine());
 
-    System.out.printf("Base imponible %8.2f\n", baseImponible);
+    System.out.printf("Base imponible %8.2f\n", baseImponible );
     System.out.printf("IVA            %8.2f\n", (baseImponible * 0.21));
     System.out.printf("-------------------\n");
     System.out.printf("Total          %8.2f\n", (baseImponible * 1.21));
