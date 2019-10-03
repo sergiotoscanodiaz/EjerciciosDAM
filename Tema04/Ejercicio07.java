fbcd93fc07382c9eaf3f9e785ff4e298cf84c591
Alan Brito Delgado <alan.brito.delgado.1972@gmail.com>
2019-10-03 09:06:53 +0200

    Subiendo Ejercicio07

---
+++ Tema04/Ejercicio07.java
@@ -0,0 +1,31 @@
+public class Ejercicio07 {
+  public static void main(String[] args) {
+
+    System.out.print("Este programa calcula la media de tres notas. Por favor, introduzca la primera nota: ");
+    Double nota1 = Double.parseDouble(System.console().readLine());
+    
+    System.out.print("Introduzca la segunda nota: ");
+    Double nota2 = Double.parseDouble(System.console().readLine());
+    
+    System.out.print("Introduzca la tercera nota: ");
+    Double nota3 = Double.parseDouble(System.console().readLine());
+    
+    double media = (nota1 + nota2 + nota3)/3 ;
+    
+    System.out.println("Tu nota media es = " + media);
+	
+	if ((media >= 0) && (media <= 4)) {
+      System.out.println("Sigue estudiando, ¡tú puedes!");
+    }
+        
+    if ((media >= 5) && (media <= 8)) {
+      System.out.println("¡Has aprobado!");
+    }
+      
+    if ((media >= 9) && (media <= 10)) {
+      System.out.println("¡Enhorabuena!");
+	
+	
+	  }
+   }
+}
