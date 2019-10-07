f6981c9999d63ba3741154901fa506ecb22d17a1
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-07 09:15:53 +0200

    Actualizando usuario

--- Tema04/Ejercicio09.java
+++ Tema04/Ejercicio09.java
@@ -1,8 +1,7 @@
 public class Ejercicio09 {
   public static void main(String[] args) {
 
-    System.out.print("Este programa realiza ecuaciones de segundo grado del tipo ax² + bx + c = 0. Para empezar introduce el valor de a: ");
-   
+    System.out.print("Este programa realiza ecuaciones de segundo grado del tipo ax² + bx + c = 0. Para empezar introduce el valor de a: ");   
     Double a = Double.parseDouble(System.console().readLine());
     
     System.out.print("Introduce el valor de b: ");   
@@ -13,8 +12,7 @@ public class Ejercicio09 {
       
     System.out.println("x = " + (-b + Math.sqrt(b * b - 4*(a * c))) / (2 * a)) ;
     
-    System.out.println("x2 = " + (-b - Math.sqrt(b * b - 4*(a * c))) / (2 * a)) ;
-   
-   }
+    System.out.println("x2 = " + (-b - Math.sqrt(b * b - 4*(a * c))) / (2 * a)) ;   
+    }
 }
 
--- Tema04/Ejercicio12.java
+++ Tema04/Ejercicio12.java
@@ -14,7 +14,7 @@ public class Ejercicio12 {
       puntos++;
       
     
-    System.out.println("1.¿Cuál es el lenguaje de programación que está ganando popularidad últimamente? ");
+    System.out.println("2.¿Cuál es el lenguaje de programación que está ganando popularidad últimamente? ");
     System.out.println("a) C++ b) Python c) Java");
     System.out.print("Escribe la respuesta: ");
     respuesta = System.console().readLine();
@@ -22,7 +22,7 @@ public class Ejercicio12 {
       puntos++;     
     }
     
-    System.out.println("2.¿Cuál es el lenguaje utilizado para la edición de páginas web? ");
+    System.out.println("3.¿Cuál es el lenguaje utilizado para la edición de páginas web? ");
     System.out.println("a)C# b)HTML 8 c)CSS");
     System.out.print("Escribe la respuesta: ");
     respuesta = System.console().readLine();
@@ -30,7 +30,7 @@ public class Ejercicio12 {
       puntos++;
     }
     
-    System.out.println("3.¿Cuál de los siguientes tipos de datos de Java tiene más precisión? ");
+    System.out.println("4.¿Cuál de los siguientes tipos de datos de Java tiene más precisión? ");
     System.out.println("a)float b)double c)int");
     System.out.print("Escribe la respuesta: ");
     respuesta = System.console().readLine();
@@ -38,7 +38,7 @@ public class Ejercicio12 {
       puntos++;
     }
     
-    System.out.println("4.¿Cuál de los siguientes tipos de datos de Java es el utilizado para una cadena de caracteres? ");
+    System.out.println("5.¿Cuál de los siguientes tipos de datos de Java es el utilizado para una cadena de caracteres? ");
     System.out.println("a)String b)double c)int");
     System.out.print("Escribe la respuesta: ");
     respuesta = System.console().readLine();
@@ -46,7 +46,7 @@ public class Ejercicio12 {
       puntos++;
     }
     
-    System.out.println("5.¿Cuál es el lenguaje que se utiliza para hacer consultas en las bases de datos?");
+    System.out.println("6.¿Cuál es el lenguaje que se utiliza para hacer consultas en las bases de datos?");
     System.out.println("a)SQL b)HTML c)Intel");
     System.out.print("Escribe la respuesta: ");
     respuesta = System.console().readLine();
@@ -54,7 +54,7 @@ public class Ejercicio12 {
       puntos++;
     }
     
-    System.out.println("6.¿Cuál es la etiqueta usada para la separación por párrafos en HTML?");
+    System.out.println("7.¿Cuál es la etiqueta usada para la separación por párrafos en HTML?");
     System.out.println("a)<br> b)<h1></h1> c)<p></p>");
     System.out.print("Escribe la respuesta: ");
     respuesta = System.console().readLine();
@@ -62,7 +62,7 @@ public class Ejercicio12 {
       puntos++;
     }
     
-    System.out.println("7.¿ En qué directorio se encuentran los archivos de configuración de Linux?");
+    System.out.println("8.¿ En qué directorio se encuentran los archivos de configuración de Linux?");
     System.out.println("a)/etc b)/linux c)/config");
     System.out.print("Escribe la respuesta: ");
     respuesta = System.console().readLine();
@@ -70,7 +70,7 @@ public class Ejercicio12 {
       puntos++;
     }
     
-    System.out.println("8. ¿Cuál de las siguientes memorias es volátil?");
+    System.out.println("9. ¿Cuál de las siguientes memorias es volátil?");
     System.out.println("a) RAM b) EPROM c) ROM"); 
     System.out.print("Escribe la respuesta: ");
     respuesta = System.console().readLine();
@@ -78,7 +78,7 @@ public class Ejercicio12 {
       puntos++;
     }
     
-    System.out.println("9. Para insertar un hiperenlace en una página se utiliza la etiqueta...");
+    System.out.println("10. Para insertar un hiperenlace en una página se utiliza la etiqueta...");
     System.out.println("a) href b) a c) link"); 
     System.out.print("Escribe la respuesta: ");
     respuesta = System.console().readLine();
@@ -86,7 +86,7 @@ public class Ejercicio12 {
       puntos++;
     }
     
-    System.out.println("10. Aplicación usada para trabajar con bases de datos en local: ");
+    System.out.println("11. Aplicación usada para trabajar con bases de datos en local: ");
     System.out.println("a) XAMPP b) Workbench c) Visual Studio Code"); 
     System.out.print("Escribe la respuesta: ");
     respuesta = System.console().readLine();
