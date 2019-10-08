ac8bce6afac5ac24a732676a732f6f59cc0679a1
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-08 17:22:17 +0200

    Actualizando Ejercicio09

--- Tema04/Ejercicio09.java
+++ Tema04/Ejercicio09.java
@@ -8,11 +8,39 @@ public class Ejercicio09 {
     Double b = Double.parseDouble(System.console().readLine());
     
     System.out.print("Introduce el valor de c: ");   
-    Double c = Double.parseDouble(System.console().readLine());
-      
-    System.out.println("x = " + (-b + Math.sqrt(b * b - 4*(a * c))) / (2 * a)) ;
+    Double c = Double.parseDouble(System.console().readLine());    
     
-    System.out.println("x2 = " + (-b - Math.sqrt(b * b - 4*(a * c))) / (2 * a)) ;   
+    if ((a == 0) && (b == 0) && (c == 0)) {
+      System.out.println("La ecuación tiene infinitas soluciones.");
     }
+      
+    
+    if ((a == 0) && (b == 0) && (c != 0)) {
+      System.out.println("La ecuación no tiene solución.");
+    }
+      
+    
+    if ((a != 0) && (b != 0) && (c == 0)) {
+      System.out.println("x1 = 0");
+      System.out.println("x2 = " + (-b / a));
+    }
+    
+    if ((a == 0) && (b != 0) && (c != 0)) {
+      System.out.println("x1 = x2 = " + (-c / b));
+    }
+    
+    if ((a != 0) && (b != 0) && (c != 0)) {
+        
+      double discriminante = b*b - (4 * a * c);
+      
+      if (discriminante < 0) {
+          System.out.println("La ecuación no tiene soluciones reales");
+      }  else {
+        System.out.println("x1 = " + (-b + Math.sqrt(discriminante))/(2 * a));
+        System.out.println("x2 = " + (-b - Math.sqrt(discriminante))/(2 * a));
+      }
+    }
+
+  }
 }
 
