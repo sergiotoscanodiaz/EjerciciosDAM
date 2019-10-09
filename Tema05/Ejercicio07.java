699bd324f11a2345e28ea741e07f1ccfedc54183
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-09 11:11:56 +0200

    Subiendo Ejercicio07 actualizado

--- Tema05/Ejercicio07.java
+++ Tema05/Ejercicio07.java
@@ -5,19 +5,21 @@ public class Ejercicio07 {
 	System.out.println("Realiza el control de acceso a una caja fuerte. La combinación será un número de 4 cifras. El programa nos pedirá la combinación para abrirla.");
 	System.out.println("Por favor, introduzca la combinación. Recuerda que debe ser un número de cuatro cifras: ");
 	int oportunidades = 4;
-	
-	 do{
+		
+	  do{
 		
 		int i = Integer.parseInt(System.console().readLine());
        
           if(i == 2407){
             System.out.println("La caja fuerte se ha abierto satisfactoriamente");
-            break;     
+            break;
+       
           } else {
 		    System.out.println("Lo siento, esa no es la combinación. ¡Prueba otra vez!");
-		    oportunidades --;
-          } while ((oportunidades > 0) && (oportunidades < 4));
-          	             	  
-	  }		
-    }
-  }  
+          }
+          oportunidades --;
+	    } 
+	     while ((oportunidades > 0) && (oportunidades < 4));	  	    		  
+    }  	    
+  }      
+    
