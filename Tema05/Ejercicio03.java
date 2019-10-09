b4d3d90542b9c5f7633779a03825dd69a4026640
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-09 10:17:07 +0200

    Subiendo Ejercicio02 y Ejercicio03

--- Tema05/Ejercicio03.java
+++ Tema05/Ejercicio03.java
@@ -1,12 +1,14 @@
-public class Ejercicio02 {
+public class Ejercicio03 {
 
   public static void main(String[] args) {
 	
 	int i = 0;
 	
-	  while (i <=100){
+	  do{
       System.out.println(i);
       i = i + 5;
-	  } 	    
+	  } 
+	   while (i <=100);
+	   	    
   } 
 }
