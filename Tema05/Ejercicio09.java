76b51c6656450b4bff3cd54d98eee9809b54f4fa
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-10 09:29:34 +0200

    Subiendo Ejercicio09

---
+++ Tema05/Ejercicio09.java
@@ -0,0 +1,20 @@
+public class Ejercicio09 {
+
+  public static void main(String[] args) {
+   
+    int numeroDeDigitos = 1, n, num;
+
+    System.out.print("Introduzca un número entero y verás cuántos dígitos tiene: ");
+    num = Integer.parseInt(System.console().readLine());
+    
+    n = num;
+    
+    while (n > 10) {
+      n /= 10;
+      numeroDeDigitos++;
+    }
+    
+    System.out.println(num + " tiene " + numeroDeDigitos + " cifras.");
+
+  }
+}
