78338c92ae0d2e6f45da201ebc6d817a3f473c85
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-13 11:46:57 +0200

    Subiendo Ejercicio10

--- Tema05/Ejercicio10.java
+++ Tema05/Ejercicio10.java
@@ -4,7 +4,7 @@ public class Ejercicio10 {
         
     int numeros = 0;
     int numeroIntroducido = 0;
-    int suma = 0;
+    int sum = 0;
 
     System.out.println("Este programa calcula la media de los números positivos introducidos.");
     System.out.println("El programa para introduciendo un número negativo):");
@@ -12,9 +12,9 @@ public class Ejercicio10 {
     while (numeroIntroducido >= 0) {
       numeroIntroducido = Integer.parseInt(System.console().readLine());
       numeros++;
-      suma += numeroIntroducido;
+      sum += numeroIntroducido;
     }
     
-    System.out.println("La media de los números positivos introducidos es " + (suma - numeroIntroducido)/ (numeros - 1));
+    System.out.println("La media de los números positivos introducidos es " + (sum - numeroIntroducido)/ (numeros - 1));
   }
 }
