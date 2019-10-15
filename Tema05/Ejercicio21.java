a6546a489c62c904a22e28e4c22c91a60d60f603
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-15 17:05:29 +0200

    Actualizando y subiendo ejercicios nuevos

--- Tema05/Ejercicio21.java
+++ Tema05/Ejercicio21.java
@@ -1,21 +1,33 @@
 public class Ejercicio21 {
 
   public static void main(String[] args) {
-  
-    int numeros = 0;
+    
+    int numeros = 0; 
     int numeroIntroducido = 0;
-    int sum = 0;
+    int numeroNumImp = 0;
+    int sumImp = 0;
+    int par = 0;
 
-    System.out.println("Este programa calcula la media de los números positivos introducidos.");
+    System.out.println("Este programa calcula la media de los números positivos impares introducidos.");
     System.out.println("El programa para introduciendo un número negativo):");
 
     while (numeroIntroducido >= 0) {
       numeroIntroducido = Integer.parseInt(System.console().readLine());
-      numeros++;
-      sum += numeroIntroducido;
-    }
+      
+      if(numeroIntroducido >=0) {
+		  numeros++;
+		  if((numeroIntroducido % 2) == 1) {
+			  sumImp += numeroIntroducido;
+			  numeroNumImp++;
+		  } else {
+			  if (numeroIntroducido > par)
+			    par = numeroIntroducido;			  
+          }
+	  }
+  }
     
-    System.out.println("La media de los números positivos introducidos es " + (sum - numeroIntroducido)/ (numeros - 1));
-  
+    System.out.println("El usuario ha introducido " + numeros + " numeros");
+    System.out.println("La media de los impares es " +sumImp / numeroNumImp);
+    System.out.println("El numero par máximo es el " + par);
   }
 }
