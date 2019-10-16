d92f5e34402df0447bee1bc4071993eddd09ae62
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-16 10:09:42 +0200

    Empezando con el Ejercicio26

---
+++ Tema05/Ejercicio26.java
@@ -0,0 +1,14 @@
+public class Ejercicio26 {
+
+  public static void main(String[] args) {
+  
+    System.out.println("Introduzca un número: ");
+    int numero = Integer.parseInt(System.console().readLine());
+    
+    System.out.println("Introduzca un dígito: ");
+    int dígito = Integer.parseInt(System.console().readLine());
+    
+    
+   
+  }
+}
