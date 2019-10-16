9b2d11c2bf93ebfbb5f16e4c4bed057cc8db0132
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-16 10:51:46 +0200

    Subiendo Ejercicio26

--- Tema05/Ejercicio26.java
+++ Tema05/Ejercicio26.java
@@ -1,21 +1,38 @@
 public class Ejercicio26 {
 
   public static void main(String[] args) {
-  
-    System.out.println("Introduzca un número: ");
-    int numero = Integer.parseInt(System.console().readLine());
     
-    System.out.println("Introduzca un dígito: ");
+    System.out.print("Introduzca un número : ");
+    int numeroIntroducido = Integer.parseInt(System.console().readLine());
+
+    System.out.print("Introduzca un dígito: ");
     int digito = Integer.parseInt(System.console().readLine());
+
+    System.out.print("El digito " + digito + " aparece dentro de " + numeroIntroducido + " en estas posiciones: ");
     
-    int posicion;
+    int numero = numeroIntroducido;
+    int volteado = 0;
+    int longitud = 0;
+    int posicion = 1;
     
-    for(posicion => 0; posicion<=10; posicion++);
-        System.out.print(posicion);
-       
-        
-    System.out.println("El dígito introducido se encuentra en la " + posicion+ " posición con respecto al " +numero);
+    if (numero == 0) {
+      longitud = 1;
+    }
     
-   
+    while (numero > 0) {
+      volteado = (volteado * 10) + (numero % 10);
+      numero /= 10;
+      longitud++;
+    } 
+    while (volteado > 0) {
+      if ((volteado % 10) == digito) {
+        System.out.print(posicion + " ");
+      }
+      volteado /= 10;
+      posicion++;
+    } 
+
+    System.out.println();
   }
 }
+
