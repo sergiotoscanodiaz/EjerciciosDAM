bd905613d582a5a7564a1c1afc9b42e900447e84
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-16 10:19:41 +0200

    Empezando con el Ejercicio26

--- Tema05/Ejercicio26.java
+++ Tema05/Ejercicio26.java
@@ -8,6 +8,11 @@ public class Ejercicio26 {
     System.out.println("Introduzca un dígito: ");
     int dígito = Integer.parseInt(System.console().readLine());
     
+    int posicion = 0;
+    
+    System.out.println("El dígito introducido se encuentra en la " + posicion+ "posición con respecto al " +numero);
+    
+    
     
    
   }
