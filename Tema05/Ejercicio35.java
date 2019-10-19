b94613ef5a86cca02013c6acc04fec30fc37c81a
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-19 19:53:09 +0200

    Subiendo Ejercicio36

---
+++ Tema05/Ejercicio35.java
@@ -0,0 +1,7 @@
+public class Ejercicio35 {
+
+  public static void main(String[] args) {
+
+   
+  }
+}
---
+++ Tema05/Ejercicio36.java
@@ -0,0 +1,22 @@
+public class Ejercicio36 {
+
+  public static void main(String[] args) {
+
+    System.out.print("Por favor, introduzca un número entero positivo: ");
+    long numeroIntroducido = Long.parseLong(System.console().readLine());
+
+    long numero = numeroIntroducido;
+    long volteado = 0;
+
+    while (numero > 0) {
+      volteado = (volteado * 10) + (numero % 10);
+      numero /= 10;
+    } 
+    
+    if (volteado == numeroIntroducido) {
+      System.out.println("El " + numeroIntroducido + " es capicúa");
+    } else {
+      System.out.println("El " + numeroIntroducido + " no es capicúa");
+    }
+  }
+}
