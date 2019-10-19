8abd6ba4fb57336903222324b82bf155fcd35489
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-19 19:37:05 +0200

    Empezando Ejercicio33

---
+++ Tema05/Ejercicio33.java
@@ -0,0 +1,18 @@
+public class Ejercicio33 {
+
+  public static void main(String[] args) {
+    
+    System.out.print("Introduzca la altura de la U: ");
+    int altura = Integer.parseInt(System.console().readLine());
+    
+
+    for (int i = 1; i < altura; i++) {
+      System.out.println("*");
+    }
+
+    
+
+  }
+}
+    
+ 
