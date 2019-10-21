273beca0f24c26e2c22964abcec1e356041f3675
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-21 11:02:42 +0200

    Actualizando Ejercicio54

--- Tema05/Ejercicio53.java
+++ Tema05/Ejercicio53.java
@@ -4,14 +4,21 @@ public class Ejercicio53 {
 
     System.out.print("Introduzca la altura de la figura: ");
     int altura = Integer.parseInt(System.console().readLine());
+    int espacios = 0;
     
     for (int i = 0; i < altura; i++) {
-      for (int j = 0; j < altura - i; j++) {
-        System.out.print("*");
+      for (int j = 0; j < espacios; j++) {
+        System.out.print(" ");
       }
-      System.out.println();
+           
+    for (int j = 0; j < altura - i; j++) {
+      System.out.print("*");
+    
+     System.out.println();
+     espacios++;
+     
     }
   }
-  
-}
+     
+
 
