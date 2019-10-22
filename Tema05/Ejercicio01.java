4d3e97e92db3ce5f6ae84ad584820571af9d305c
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-22 19:27:17 +0200

    Actualización

--- Tema05/Ejercicio01.java
+++ Tema05/Ejercicio01.java
@@ -2,8 +2,8 @@ public class Ejercicio01 {
 
   public static void main(String[] args) {
 	
-	for (int i = 0; i <=100; i = i + 5) {
+	  for (int i = 0; i <=100; i = i + 5) {
       System.out.println(i);
-	} 
+	  } 
   } 
 }
