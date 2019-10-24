e5c051c9c5bb687bbbcec99ddce8fcbafb48067f
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-24 19:10:32 +0200

    Subiendo Ejercicio 58

---
+++ Tema05/Ejercicio58.java
@@ -0,0 +1,22 @@
+public class Ejercicio58 {
+
+  public static void main(String[] args) {
+   
+    System.out.print("Introduzca un número entero: ");
+    long numeroIntroducido = Integer.parseInt(System.console().readLine());
+    
+    int suma = 0;
+    int longitud = 0;
+    
+    while (numeroIntroducido > 0) {
+      suma += numeroIntroducido % 10;
+      numeroIntroducido /= 10;
+      longitud++;
+    }
+
+    System.out.println("La media de sus dígitos es " + (double)suma / longitud);
+  }
+  
+}
+
+ 
