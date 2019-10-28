0e3f6e6ad35e3be6193e80f1adb37bc6578ed3ab
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-28 10:42:59 +0100

    Subiendo Ejercicio05

--- Tema06/Ejercicio05.java
+++ Tema06/Ejercicio05.java
@@ -5,19 +5,27 @@ public class Ejercicio05 {
     System.out.println("Este es un programa que muestra 50 números enteros aleatorios entre 100 y 199 (ambos incluidos)");
     
     int suma = 0;
-    int min = 0;
-    int max = 0;
-    int aux = 0;
-         
+    int min = 199;
+    int max = 100;
+    int numero;
+    
       for (int i = 1; i <= 50; i++) {
-        int numero = ((int)(Math.random()*100) + 100);    
+        numero = ((int)(Math.random()*100) + 100);    
         System.out.print(numero+" ");             
         suma += numero; 
         
+         if (numero < min) {
+           min = numero;
+         }
+      
+         if (numero > max) {
+           max = numero;
+         }
       }
       System.out.println();
       
-      System.out.print("Media: " +suma/50);
-      
+      System.out.println("Media: " +suma/50);
+      System.out.println("El máximo es: " +max);
+      System.out.println("El mínimo es: " +min);
   }
 }
