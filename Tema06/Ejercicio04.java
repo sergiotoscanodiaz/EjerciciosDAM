86c28aba516c31f9a782ebde10070ca5e49e5998
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-28 10:10:56 +0100

    Subiendo Ejercicio04

---
+++ Tema06/Ejercicio04.java
@@ -0,0 +1,12 @@
+public class Ejercicio04 {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Este es un programa que muestra 20 números enteros aleatorios entre 0 y 10 (ambos incluidos)");
+         
+        for (int i = 1; i <= 20; i++) {
+          int numero = ((int)(Math.random()*10) + 1);    
+          System.out.print(numero+" ");              
+        }
+  }
+}
