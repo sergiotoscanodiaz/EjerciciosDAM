fd70d1b734fd4c6062e661e6aacf6565155d0995
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-28 11:04:19 +0100

    Subiendo Ejercicio06

--- Tema06/Ejercicio06.java
+++ Tema06/Ejercicio06.java
@@ -2,30 +2,20 @@ public class Ejercicio06 {
 
   public static void main(String[] args) {
     
-    System.out.println("Este es un programa que muestra 50 números enteros aleatorios entre 100 y 199 (ambos incluidos)");
+    System.out.println("Este es un programa que piensa un número al azar entre 0 y 100. Introduzca un número: ");
     
-    int suma = 0;
-    int min = 199;
-    int max = 100;
-    int numero;
+    String numeroIntroducido;
+    int numero = ((int)(Math.random()*101));
     
-      for (int i = 1; i <= 50; i++) {
-        numero = ((int)(Math.random()*100) + 100);    
-        System.out.print(numero+" ");             
-        suma += numero; 
+      for (int i = 1; i <= 5; i++) {
+        numeroIntroducido = System.console().readLine();
+        int n = Integer.parseInt(numeroIntroducido);  
         
-         if (numero < min) {
-           min = numero;
-         }
-      
-         if (numero > max) {
-           max = numero;
-         }
-      }
-      System.out.println();
-      
-      System.out.println("Media: " +suma/50);
-      System.out.println("El máximo es: " +max);
-      System.out.println("El mínimo es: " +min);
+        if (n == numero){
+          System.out.println("¡Has acertado!");
+        }else{
+          System.out.println("¡Inténtalo otra vez!");                
+        }
+      } 
   }
 }
