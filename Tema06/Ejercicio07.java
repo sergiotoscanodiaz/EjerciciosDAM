30596e2abab60d6fec4a8cc45d9ca5b9d4541ad7
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-31 08:43:03 +0100

    Subiendo Ejercicio07

--- Tema06/Ejercicio07.java
+++ Tema06/Ejercicio07.java
@@ -3,26 +3,34 @@ public class Ejercicio07 {
   public static void main(String[] args) {
     
     System.out.println("Este es un programa que muestra tres apuestas de la quiniela: ");
+     
+    int quiniela;
+    int columnas = 3;
     
-     for (int i = 1; i <= 14; i++) {
-       int quiniela = ((int)(Math.random()*3) + 1);    
-       
-       switch (quiniela) {         
-        
-         case 1:
-           System.out.println(1); 
-         break;
-         case 2:
-           System.out.println("x"); 
-         break;
-         case 3:
-           System.out.println(2);
-         break;
-         
-       }
-                     
-     }
-    
-   
+    for (int fila = 1; fila <= 15; fila++) {
+      System.out.printf("%4d. |", fila);
+      
+      if (fila == 15) {
+        columnas = 1;
+      }
+      
+      for (int apuesta = 1; apuesta <= columnas; apuesta++) {
+        quiniela = (int)(Math.random() * 3) + 1;
+        switch(quiniela) {
+          case 1:
+            System.out.print("1  |");
+            break;
+          case 2:
+            System.out.print("  2|");
+            break;
+          case 3:
+            System.out.print(" X |");
+          default:
+        }
+      }
+      System.out.println();
+    }
   }
 }
+
+
