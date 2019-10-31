8bd49e67f7d538251dfed649f7b71634eeb374a1
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-10-31 08:49:25 +0100

    Empezando Ejercicio08

---
+++ Tema06/Ejercicio08.java
@@ -0,0 +1,34 @@
+public class Ejercicio08 {
+
+  public static void main(String[] args) {
+    
+    System.out.println("Este es un programa que muestra tres apuestas de la quiniela: ");
+     
+    int quiniela;
+    int columnas = 3;
+    
+    for (int fila = 1; fila <= 15; fila++) {
+      System.out.printf(" |", fila);
+      
+      if (fila == 15) {
+        columnas = 1;
+      }
+      
+      for (int apuesta = 1; apuesta <= columnas; apuesta++) {
+        quiniela = (int)(Math.random() * 3) + 1;
+        switch(quiniela) {
+          case 1:
+            System.out.print("1  |");
+            break;
+          case 2:
+            System.out.print("  2|");
+            break;
+          case 3:
+            System.out.print(" X |");
+          default:
+        }
+      }
+      System.out.println();
+    }
+  }
+}
