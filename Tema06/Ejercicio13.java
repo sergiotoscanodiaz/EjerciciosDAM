dde5be8fb01da77f24b5c8690466ff8e1a4da600
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-11-03 19:12:59 +0100

    Subiendo Ejercicio13

---
+++ Tema06/Ejercicio13.java
@@ -0,0 +1,14 @@
+public class Ejercicio13 {
+
+  public static void main(String[] args) {
+
+    int dado1;
+    int dado2;
+    
+    do {
+      dado1 = (int)(Math.random() * 6) + 1;
+      dado2 = (int)(Math.random() * 6) + 1;
+      System.out.println(dado1 + " " + dado2);
+    } while (dado1 != dado2);
+  }
+}
