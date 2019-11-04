e26e7c9e407be89a55e10407ce5866f2e68fd1b6
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-11-04 10:32:15 +0100

    Actualizando Ejercicio14

--- Tema06/Ejercicio14.java
+++ Tema06/Ejercicio14.java
@@ -4,14 +4,17 @@ public class Ejercicio14 {
     
     System.out.println("Este es un programa que piensa un número al azar entre 0 y 100. Introduzca un número: ");
     
-    String numeroIntroducido;
-    int numero = ((int)(Math.random()*101));
+    String respuesta;
+    
+    for (int i=1; i<=5; i++){
+    int numero = ((int)(Math.random()*100)+1);
     System.out.println("¿Es este el número que has pensado? s/n " +numero);
-    String respuesta = System.console().readLine();
+    respuesta = System.console().readLine();
     
-    if (respuesta.equals("s"));
-      System.out.println("¡Perfecto!");
-    
-       
-  }
+      if(respuesta.equals("s")){
+        System.out.println("¡Eres un máquina!");
+      }
+    }
+  }    
 }
+
