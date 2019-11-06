f9d516da169a773622bff79d2a19c80d3690aba3
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-11-06 08:55:36 +0100

    Subiendo Ejercicio20

---
+++ Tema06/Ejercicio20.java
@@ -0,0 +1,23 @@
+public class Ejercicio20 {
+  public static void main(String[] args) {
+
+    System.out.println("Este es un programa que pinta por pantalla una cuba con cierta cantidad de agua.");
+    System.out.println("Por favor, indique la capacidad de la cuba en litros: ");
+    int capacidad = Integer.parseInt(System.console().readLine());
+
+    int litros = (int)(Math.random() * (capacidad + 1));
+        
+    for (int i = 0; i < capacidad; i++) {
+      if (i < (capacidad - litros)) {
+        System.out.println("*    *");
+      } else {
+        System.out.println("*$$$$*");
+      }
+    }
+        
+    System.out.println("******"); 
+    System.out.print("La cuba tiene una capacidad de " + capacidad);
+    System.out.print(" litros y contiene " + litros + " litros de agua.");
+  }
+}
+
