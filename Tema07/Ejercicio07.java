5d43d4d1c74e03fc55eef5a66845c8b7a3977687
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-11-13 17:21:40 +0100

    Subiendo Ejercicio07 Tema07

--- Tema07/Ejercicio07.java
+++ Tema07/Ejercicio07.java
@@ -9,5 +9,25 @@ public class Ejercicio07 {
       numero[i] = (int)(Math.random() * 21);
       System.out.print(numero[i] + "  ");
     }
+    
+    System.out.print("\nIntroduzca un número de los que se han mostrado: ");
+    int valor1 = Integer.parseInt(System.console().readLine());
+    System.out.print("Introduzca el valor por el que quiere sustituirlo: ");
+    int valor2 = Integer.parseInt(System.console().readLine());
+
+    System.out.println();
+    
+    for (i = 0; i < 100; i++) {
+      if (numero[i] == valor1) {
+        numero[i] = valor2;
+        System.out.print("\"" + numero[i] + "\"  ");
+      } else {
+        System.out.print(numero[i] + "  ");
+      }
+    }
+
+    System.out.println();
   }
 }
+
+  
