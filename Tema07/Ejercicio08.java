665de4b389a441b5e71c4be92d7feb64a13150de
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-11-13 17:53:34 +0100

    Subiendo Ejercicio08 Tema07

---
+++ Tema07/Ejercicio08.java
@@ -0,0 +1,27 @@
+public class Ejercicio08 {
+
+  public static void main(String[] args) {
+  
+    String[] mes = {
+      "enero", "febrero", "marzo", "abril", "mayo", "junio",
+      "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
+    };
+                  
+    int[] temperatura = new int[12];
+    int i, j;
+    
+          
+    for (i = 0; i < 12; i++) {
+      System.out.print("Introduzca la temperatura media de " + mes[i] + ": ");
+      temperatura[i] = Integer.parseInt(System.console().readLine());
+    }
+
+    for (i = 0; i < 12; i++) {
+      System.out.printf("%10s "+ "│", mes[i]);
+      for (j = 0; j < temperatura[i]; j++) {
+        System.out.print("*");
+      }
+      System.out.println(" " + temperatura[i] + "ºC");
+    }
+  }
+}
