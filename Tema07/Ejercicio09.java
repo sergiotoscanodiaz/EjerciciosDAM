9156388f53e655d97843f272041228ecd9dd517e
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-11-13 17:58:08 +0100

    Subiendo Ejercicio09 Tema07

--- Tema07/Ejercicio09.java
+++ Tema07/Ejercicio09.java
@@ -3,15 +3,14 @@ public class Ejercicio09 {
   public static void main(String[] args) {
   
     int[] numero = new int[8];
-    int i;
     
     System.out.println("Introduzca 8 números enteros, pulse ENTER al introducir cada número:");
 
-    for (i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++) {
       numero[i] = Integer.parseInt(System.console().readLine());
     }
 
-    for (i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++) {
       if (numero[i] % 2 == 0) {
         System.out.println(numero[i] + " par");
       } else {
