7fe317f4a3840f8c8b030fc36af318c3aef5beab
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2019-11-14 09:13:14 +0100

    Subiendo Ejercicio10 Tema07

--- Tema07/Ejercicio10.java
+++ Tema07/Ejercicio10.java
@@ -9,19 +9,46 @@ public class Ejercicio10 {
   public static void main(String[] args) {
   
     int[] numero = new int[20];
+    int[] par = new int[20];
+    int[] impar = new int[20];
+    int pares = 0;
+    int impares = 0;
     
     System.out.println("Este programa genera 20 números enteros aleatorios entre 0 y 100. El programa es capaz de pasar todos los números pares a las primeras posiciones del array (del 0 en adelante) y todos los números impares a las celdas restantes.: ");
 
     for (int i = 0; i < 20; i++) {
-      numero[i] = (int)(Math.random() * 101);;
+      numero[i] = (int)(Math.random() * 101);
     }
 
     for (int i = 0; i < 20; i++) {
       if (numero[i] % 2 == 0) {
-        System.out.println(numero[i] + " par");
+        par[i] = numero[i];
       } else {
-        System.out.println(numero[i] + " impar");
+        impar[i] = numero[i];        
       }
+    }
+    
+    System.out.println("Array 1:");
+    for (int i = 0; i < 20; i++) {
+      System.out.print(numero[i] + " ");
+    }
+    System.out.println();
+    
+    for (int i = 0; i < pares; i++) {
+      numero[i] = par[i];
+    }
+       
+    for (int i = pares; i < 20; i++) {
+      numero[i] = impar[i - pares];
+    }
+    
+    System.out.println("Array resultante:");
+    for (int i = 0; i < 20; i++) {
+      System.out.print(par[i] + " ");  
+    }
+    
+    for (int i = 0; i < 20; i++) {
+      System.out.print(impar[i] + " ");  
     }
   }
 }
