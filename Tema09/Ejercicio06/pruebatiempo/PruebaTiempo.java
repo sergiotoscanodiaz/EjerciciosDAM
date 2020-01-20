b01b3f3620c3cc8d7358cbe37c1883c37453b542
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2020-01-20 10:06:07 +0100

    Subiendo Ejercicio06

---
+++ Tema09/Ejercicio06/pruebatiempo/PruebaTiempo.java
@@ -0,0 +1,16 @@
+package pruebatiempo;
+
+public class PruebaTiempo {
+
+  public static void main(String[] args) {
+    
+    Tiempo t1 = new Tiempo(18, 10, 30);
+    Tiempo t2 = new Tiempo(0, 40, 40);
+    Tiempo t3 = new Tiempo(0, 35, 20);
+        
+    System.out.println(t1 + " + " + t2 + " = " + t1.suma(t2));
+    System.out.println(t2 + " - " + t3 + " = " + t2.resta(t3));
+   
+  }
+  
+}
