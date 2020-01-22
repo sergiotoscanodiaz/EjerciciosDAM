40c1e33336fbb85c66bebfc557d5051dff066d96
Sergio Toscano Díaz <55782014+sergiotoscanodiaz@users.noreply.github.com>
2020-01-22 09:36:30 +0100

    Subiendo Ejercicios07 y 08

---
+++ Tema09/Ejercicio07/expocochescampa/ExpoCochesCampa.java
@@ -0,0 +1,51 @@
+public class ExpoCochesCampa {
+  public static void main(String[] args) {
+    
+    Zona principal = new Zona(1000);
+    Zona compraVenta = new Zona(200);
+    Zona vip = new Zona(25);
+    
+    int opcion = 0;
+    int opcion2 = 0;
+    int n = 0;
+    
+    while (opcion < 3) {
+      System.out.println("1. Mostrar número de entradas libres");
+      System.out.println("2. Vender entradas");
+      System.out.println("3. Salir");
+      
+      opcion = Integer.parseInt(System.console().readLine());
+      
+      if (opcion == 1) {
+        System.out.println("En la zona principal hay " + principal.getEntradasPorVender());
+        System.out.println("En la zona de compra venta hay " + compraVenta.getEntradasPorVender());
+        System.out.println("En la zona vip hay " + vip.getEntradasPorVender());
+      }
+      
+      if (opcion == 2) {
+        System.out.println("1. Principal");
+        System.out.println("2. Compra-venta");
+        System.out.println("3. Vip");
+        System.out.print("Elige la zona para la que quieres comprar las entradas: ");
+        
+        opcion2 = Integer.parseInt(System.console().readLine());
+        
+        System.out.print("¿Cuántas entradas quieres? ");
+        n = Integer.parseInt(System.console().readLine());
+        
+        switch (opcion2) {
+          case 1:
+            principal.vender(n);
+            break;
+          case 2:
+            compraVenta.vender(n);
+            break;
+          case 3:
+            vip.vender(n);
+            break;
+          default:
+        }
+      }
+    }
+  }
+}
