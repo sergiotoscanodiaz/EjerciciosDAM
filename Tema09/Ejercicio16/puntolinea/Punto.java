77340d164c6488a1cef823acc08992db489068a9
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2020-01-26 18:00:21 +0100

    Subiendo Ejercicio13 y 16

---
+++ Tema09/Ejercicio16/puntolinea/Punto.java
@@ -0,0 +1,18 @@
+package puntolinea;
+
+public class Punto {
+  
+  private double x;
+  private double y;
+
+  public Punto(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  @Override
+  public String toString() {
+    return "(" + x + ", " + y + ")";
+  }
+  
+}
