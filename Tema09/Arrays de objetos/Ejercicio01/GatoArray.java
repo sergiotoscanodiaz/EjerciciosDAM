a4dced1eb538c3dd751afe973ad5999025cae3bf
Sergio Toscano Díaz <sergiotoscano2407@outlook.es>
2020-02-01 18:22:54 +0100

    Subiendo Ejercicio01

---
+++ Tema09/Arrays de objetos/Ejercicio01/GatoArray.java
@@ -0,0 +1,34 @@
+
+public class GatoArray {
+
+  public static void main(String[] args) {
+    
+    Gato[] gato = new Gato[4];
+
+    int i;
+
+    System.out.println("\nPor favor, introduzca los datos de los gatos.");
+  
+    for (i = 0; i < 4; i++) {
+      gato[i] = new Gato();
+      System.out.println("  \nGato nº " + (i + 1));
+      System.out.print("Nombre: ");
+      gato[i].setNombre(System.console().readLine());
+      System.out.print("Color: ");
+      gato[i].setColor(System.console().readLine());
+      System.out.print("Raza: ");
+      gato[i].setRaza(System.console().readLine());
+    }
+
+    System.out.println("\n\nDatos de los gatos.");
+
+    for (i = 0; i < 4; i++) {
+      System.out.println("\nGato nº" + (i + 1));
+      System.out.println("Nombre: " + gato[i].getNombre());
+      System.out.println("Color: " + gato[i].getColor());
+      System.out.println("Raza: " + gato[i].getRaza());
+    }
+    
+  }
+  
+}
