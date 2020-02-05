f48528ae3512480cb28553c346db7bec1386753d
Sergio Toscano Díaz <55782014+sergiotoscanodiaz@users.noreply.github.com>
2020-02-05 11:46:47 +0100

    Subida de ejercicios del examen simulacro.

---
+++ EJERCICIOS EXAMEN/EXAMENSIMUL/Ejercicio02/Carrito.java
@@ -0,0 +1,37 @@
+package ex18std.Ex18std2;
+
+import java.util.ArrayList;
+
+public class Carrito {
+  
+  ArrayList<Elemento> a = new ArrayList<>();
+
+  public void agrega(Elemento e) {
+      a.add(e);
+  }
+
+  public int numeroDeElementos() {
+    return a.size();
+  }
+
+  public double importeTotal() {
+    double total = 0;
+    for (Elemento e : a) {
+
+      total += e.getPrecio() * e.getCantidad();
+
+    }
+    return total;
+  }
+
+  @Override
+  public String toString() {
+    String contenido = "Contenido del carrito\n=====================\n";
+    for (Elemento e : a) {
+      contenido += e + "\n";
+    }
+    return contenido;
+  }
+   
+}
+
